f977dfe843983175935988b450b94f8d829fc925
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2019-10-20 17:12:50 +0530

    Serve static react build from express

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import graphqlHTTP from "express-graphql";
 import schema from "./src/schema/schema";
 import cors from "cors";
 import mongoose from "mongoose";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,11 @@ app.use("/graphql", graphqlHTTP({
   schema,
   graphiql: true
 }));
+
+ // All remaining requests return the React app, so it can handle routing.
+ app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log("vuePress running on port :", PORT);
